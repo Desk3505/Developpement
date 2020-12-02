950c38b828da04742b48ab61e0d206be84d4dd75
Garthinx <deemunit@gmail.com>
2020-12-02 17:39:16 -0500

    Add files via upload

---
+++ ProjetFinal/app/src/main/java/com/example/projetfinal/MyImageView.java
@@ -0,0 +1,31 @@
+package com.example.projetfinal;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.widget.ImageView;
+
+import static android.graphics.Color.TRANSPARENT;
+
+public class MyImageView extends ImageView {
+    public MyImageView(Context context){
+        super(context);
+    }
+
+    public float myX;
+    public float myY;
+    public boolean drawC = false;
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+
+        Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
+        if(drawC){
+            canvas.drawCircle(myX, myY,50,p);
+        }
+    }
+
+}
